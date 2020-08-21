d45110491219f97edee74d3b57407da64d679462
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-08-21 19:06:30 -0400

    fixing else

--- mayor_de_4.rb
+++ mayor_de_4.rb
@@ -8,14 +8,14 @@ cuarto_dato_obtenido = ARGV[3].to_i
 
 #primer dato obtenido
 if (primer_dato_obtenido > segundo_dato_obtenido && primer_dato_obtenido > tercer_dato_obtenido && primer_dato_obtenido > cuarto_dato_obtenido)
-    puts "#{primer_dato_obtenido} es mayor"
+    puts "#{primer_dato_obtenido}"
 #segundo dato obtenido
 elsif (segundo_dato_obtenido > tercer_dato_obtenido && segundo_dato_obtenido > primer_dato_obtenido && segundo_dato_obtenido > cuarto_dato_obtenido)
-    puts "#{segundo_dato_obtenido} es mayor"
+    puts "#{segundo_dato_obtenido}"
 #tercer dato obtenido
 elsif (tercer_dato_obtenido > primer_dato_obtenido && tercer_dato_obtenido > segundo_dato_obtenido && tercer_dato_obtenido > cuarto_dato_obtenido)
-    puts "#{tercer_dato_obtenido} es mayor"
+    puts "#{tercer_dato_obtenido}"
 #cuarto dato obtenido
-elsif (cuarto_dato_obtenido > primer_dato_obtenido && cuarto_dato_obtenido > segundo_dato_obtenido)
-    puts "#{cuarto_dato_obtenido} es mayor"
+else (cuarto_dato_obtenido > primer_dato_obtenido && cuarto_dato_obtenido > segundo_dato_obtenido)
+    puts "#{cuarto_dato_obtenido}"
 end
